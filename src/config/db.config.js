f44dbd2792a81fb7e2485cac051113f6835cda03
unknown <xarmzon@gmail.com>
2022-05-18 18:12:56 +0000

    setup .env

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "apex_hauz",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "",
+  database: process.env.DB_NAME || "apex_hauz",
 });
 
 connection.connect(function (err) {
