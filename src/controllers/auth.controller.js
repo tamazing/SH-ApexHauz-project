52fc5f082ceab6d4f9cf9658886f88a9f2a6b685
unknown <xarmzon@gmail.com>
2022-05-19 02:25:57 +0000

    feat: user can signin

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,8 +2,7 @@ const bcryptjs = require("bcryptjs");
 const User = require("../models/user.model.js");
 const { MESSAGES, HTTP_REQUEST_CODES } = require("../../libs/constants");
 const { createError, generateError } = require("../../libs/error.js");
-const { generateSuccessData } = require("../../libs/index.js");
-const jwt = require("jsonwebtoken");
+const { generateSuccessData, generateToken } = require("../../libs/index.js");
 
 exports.register = (req, res, next) => {
   try {
@@ -59,23 +58,13 @@ exports.register = (req, res, next) => {
           if (err) {
             createError(MESSAGES.NEW_ACCOUNT_ERROR);
           } else {
-            const token = jwt.sign(
-              {
-                id: data.id,
-                email: data.email,
-                is_admin: Boolean(data.is_admin),
-              },
-              process.env.JWT_SECRET_KEY,
-              { expiresIn: "7d" }
+            const token = generateToken(data, process.env.JWT_SECRET_KEY);
+            res.status(200).json(
+              generateSuccessData(MESSAGES.NEW_ACCOUNT_SUCCESSFUL, {
+                ...data,
+                token,
+              })
             );
-            res
-              .status(200)
-              .json(
-                generateSuccessData(MESSAGES.NEW_ACCOUNT_SUCCESSFUL, {
-                  ...data,
-                  token,
-                })
-              );
           }
         });
       }
@@ -85,8 +74,51 @@ exports.register = (req, res, next) => {
   }
 };
 
-exports.login = async (req, res, next) => {
+exports.signin = async (req, res, next) => {
   try {
+    if (!Object.keys(req.body).length) {
+      console.log("here");
+      createError(
+        MESSAGES.INVALID_REQUEST_NO_BODY,
+        HTTP_REQUEST_CODES.BAD_REQUEST
+      );
+      return;
+    }
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      createError(
+        MESSAGES.INVALID_REQUEST_FIELD_MISSING,
+        HTTP_REQUEST_CODES.BAD_REQUEST
+      );
+    }
+    User.findByEmail(email, async (_, account) => {
+      if (!account) {
+        res
+          .status(HTTP_REQUEST_CODES.BAD_REQUEST)
+          .json(generateError(MESSAGES.NO_USER));
+      } else {
+        const userPass = account.password;
+        const validPass = await bcryptjs.compare(password, userPass);
+        if (!validPass) {
+          res
+            .status(HTTP_REQUEST_CODES.BAD_REQUEST)
+            .json(generateError(MESSAGES.LOGIN_ERROR));
+          return;
+        } else {
+          const data = account;
+          delete data.password;
+          const token = generateToken(data, process.env.JWT_SECRET_KEY);
+
+          res.status(200).json(
+            generateSuccessData(MESSAGES.LOGIN_SUCCESSFUL, {
+              token,
+              ...data,
+            })
+          );
+        }
+      }
+    });
   } catch (err) {
     next(err);
   }
